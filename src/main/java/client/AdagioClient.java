7e7a1217d81f870a06ff0d73d4378e1aec7c9452
Marc Babtist <marcbabtist@gmail.com>
2017-02-16 15:43:41 +0100

    Added .idea to .gitignore and removed from repo

---
+++ src/main/java/client/AdagioClient.java
@@ -0,0 +1,7 @@
+package client;
+
+/**
+ * Created by Bab on 16-2-2017.
+ */
+public class AdagioClient {
+}
---
+++ src/main/java/configuration/Config.java
@@ -0,0 +1,7 @@
+package configuration;
+
+/**
+ * Created by Bab on 16-2-2017.
+ */
+public class Config {
+}
