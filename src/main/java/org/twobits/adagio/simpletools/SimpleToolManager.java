3a0b3b16a826e7a723cdf27166a026b0c497d5e3
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2017-05-12 16:16:35 +0200

    !roll now supports multipe dice types (1d8+2d6)

--- src/main/java/org/twobits/adagio/simpletools/SimpleToolManager.java
+++ src/main/java/org/twobits/adagio/simpletools/SimpleToolManager.java
@@ -1,22 +1,28 @@
 package org.twobits.adagio.simpletools;
 
-import java.util.Random;
-
 /**
  * Created by jeroen on 12-5-17.
  */
 public class SimpleToolManager {
 
     public int rollDice(String content) throws IllegalArgumentException {
-        String[] parts = content.split("d");
+        String[] dice = content.split("\\+");
 
-        if (parts.length != 2){
-            throw new IllegalArgumentException("Invalid parameters. Please use 'xdy' where x and y are integers.");
+        if (dice.length < 1){
+            throw new IllegalArgumentException("Invalid parameters. Example: 'xdy' where a,b,x and y are integers.");
         }
 
         int total = 0;
-        for (int i = 0; i < Integer.parseInt(parts[0]); i++) {
-            total += Math.random() * Integer.parseInt(parts[1]) + 1;
+        for (int j = 0; j < dice.length; j++) {
+            String[] parts = dice[j].split("d");
+
+            if (parts.length != 2) {
+                throw new IllegalArgumentException("Invalid parameters. Example: 'xdy+adb' where a,b,x and y are integers.");
+            }
+
+            for (int i = 0; i < Integer.parseInt(parts[0]); i++) {
+                total += Math.random() * Integer.parseInt(parts[1]) + 1;
+            }
         }
 
         return total;
